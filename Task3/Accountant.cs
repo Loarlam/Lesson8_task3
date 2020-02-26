eaac99044dfce84f9a58fe438c14eac7a99b436d
Loarlam <messiah02@mail.ru>
2020-02-26 21:29:14 +0300

    Нужно убрать switch из метода Main()

---
+++ Task3/Accountant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task3
+{
+    class Accountant
+    {
+        public bool AskForBonus(Post worker, int hours)
+        {
+            switch (worker)
+            {
+                case Post.tester:
+                    if (hours > (int)Post.tester)
+                        Console.WriteLine($"\nДля {worker} положена премия.");
+                    else
+                        Console.WriteLine($"\nВ этом месяце {worker} без премии.");
+                    break;
+                case Post.programmer:
+                    if (hours > (int)Post.programmer)
+                        Console.WriteLine($"\nДля {worker} положена премия.");
+                    else
+                        Console.WriteLine($"\nВ этом месяце {worker} без премии.");
+                    break;
+                case Post.manager:
+                    if (hours > (int)Post.manager)
+                        Console.WriteLine($"\nДля {worker} положена премия.");
+                    else
+                        Console.WriteLine($"\nВ этом месяце {worker} без премии.");
+                    break;
+                default:
+                    break;
+            }
+            return false;
+        }
+    }
+}
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -7,10 +7,46 @@ using System;
 
 namespace Task3
 {
+    enum Post
+    {
+        tester = 160,
+        programmer = 124,
+        manager = 140
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Accountant accountant;
+            byte myProfession = 0;
+            int hoursWorkedInMonth = 0;
+
+            Console.Write("1 - тестировщик\n2 - программист\n3 - менеджер\nМой ответ = ");
+            myProfession = Byte.Parse(Console.ReadLine());
+
+            Console.Write("\nКоличество отработанных часов в месяц = ");
+            hoursWorkedInMonth = Int32.Parse(Console.ReadLine());
+
+            accountant = new Accountant();
+
+            switch (myProfession)
+            {
+                case 1:
+                    accountant.AskForBonus(Post.tester, hoursWorkedInMonth);
+                    break;
+                case 2:
+                    accountant.AskForBonus(Post.programmer, hoursWorkedInMonth);
+                    break;
+                case 3:
+                    accountant.AskForBonus(Post.manager, hoursWorkedInMonth);
+                    break;
+                default:
+                    Console.WriteLine("Ошибка! Профессия отсутствует в списке.");
+                    break;
+            }
+
+            Console.ReadKey();
         }
     }
 }
